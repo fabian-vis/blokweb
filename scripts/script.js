29a119ca6f418287a43338bfd9b0eae2741db4c4
fabian-vis <70572501+fabian-vis@users.noreply.github.com>
2020-09-15 17:49:01 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-// JavaScript Document
+
+var deButton = document.querySelector("menu");
+
+function doeFormNeerEnOp(){
+	let hetFormulier = document.querySelector("onzichtbaar");
+	hetFormulier.classList.toggle("toonMenu");
+}
+
+deButton.addEventListener("click", doeFormNeerEnOp);
